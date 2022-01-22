8f0709671c34ed0ced524e6592dcb1eb8f71c6d0
Mohamed Sayed <42018173@hti.edu.eg>
2022-01-22 13:28:30 +0200

    feat: Setup Project
    - Add Home Screen 
    - Delete StoryBoard

--- Moviesta/Scenes/HomeMoviesScreen/ViewModel/Controller/HomeMoviesController.swift
+++ Moviesta/Scenes/HomeMoviesScreen/ViewModel/Controller/HomeMoviesController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeMoviesController.swift
 //  Moviesta
 //
 //  Created by SODA on 22/01/2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeMoviesController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
     }
 
 
 }
-
